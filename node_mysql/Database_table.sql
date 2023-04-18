4b3f13ed62a4ad2586932d49c40012b4b28f10df
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-04-17 21:49:53 -0300

    Database Node and MySQL

--- node_mysql/Database_table.sql
+++ node_mysql/Database_table.sql
@@ -8,4 +8,13 @@ CREATE TABLE books (
 );
 
 delete from books where i_id_books > 0;
+select * from books where s_title_books like("%que%");
 select * from books;
+
+update books set 
+	s_title_books = "ttt",
+    s_pageqtd_books = 10000
+where i_id_books = 5;
+
+
+delete from books where i_id_books = 12;
