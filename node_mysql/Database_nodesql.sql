030bb2739a321680f1a3b6bf9fa0ec171c4d5a79
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-04-26 11:23:14 -0300

    Teste

--- node_mysql/Database_nodesql.sql
+++ node_mysql/Database_nodesql.sql
@@ -3,6 +3,9 @@ CREATE DATABASE nodesequelize;
 CREATE TABLE IF NOT EXISTS `Users` (`id` INTEGER NOT NULL auto_increment , `name` VARCHAR(255) NOT NULL, `occupation` VARCHAR(255), `newsletter` TINYINT(1), `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB;
 
 SELECT * FROM users;
-
-
+delete from addresses where id > 0;
+select * from addresses;
 delete from users where id > 0;
+
+select * from users U
+inner join addresses a on a.UserId = U.id;
