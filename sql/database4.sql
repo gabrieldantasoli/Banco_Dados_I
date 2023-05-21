b613b6b3c6dcd578d52020e1066a6a8055f97aa9
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-05-21 16:19:34 -0300

    Database

--- sql/database4.sql
+++ sql/database4.sql
@@ -1,3 +1,41 @@
-CREATE DATABASE toughts;
+CREATE DATABASE firma;
 
-SELECT * FROM toughts;
+CREATE TABLE firma.empregado (
+	matricula INT NOT NULL,
+    nome varchar(50) NOT NULL,
+    endereco varchar(50),
+    salario FLOAT NOT NULL,
+    supervisor INT,
+    depto INT not null,
+    PRIMARY KEY (matricula),
+    check (salario >= 0),
+    FOREIGN KEY (supervisor) references firma.empregado(matricula),
+    FOREIGN KEY (depto) references firma.departamento(coddep)
+);
+
+CREATE TABLE firma.departamento(
+	coddep INT NOT NULL,
+    nome varchar(25) not null,
+    gerente INT,
+    datainit DATE,
+    PRIMARY KEY (coddep)
+);
+
+CREATE TABLE firma.projeto(
+	codproj int not null,
+    nome varchar(20) not null,
+    local varchar(20) not null,
+	depart int not null,
+    primary key (codproj),
+    FOREIGN KEY (depart) references firma.departamento(coddep)
+);
+
+CREATE TABLE firma.alocacao(
+	matric int not null,
+    codproj int not null,
+    horas float not null,
+    PRIMARY KEY (matric),
+    FOREIGN KEY (codproj) references firma.projeto(codproj)
+);
+
+DROP TABLE firma.projeto;
