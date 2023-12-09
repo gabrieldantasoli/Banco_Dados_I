73b9884019057f911058fbb1bf1583108d473d83
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-12-09 13:00:38 -0300

    FUNCTIONS - SINGLE ROW

--- oracle/Aula2/1.function_single_row.sql
+++ oracle/Aula2/1.function_single_row.sql
@@ -12,6 +12,13 @@
         + LPAD | RPAD
         + TRIM
         + REPLACE
+
+    NUMBER-MANIPULATION:
+        + ROUND
+        + TRUNC -> TRUNCATE in mySQL
+        + MOD
+        + ABS
+        + SQLT
 */
 
 SELECT 
@@ -70,3 +77,47 @@ SELECT
 	c.codcli as COD,
     REPLACE(concat(c.nome, " ", c.sobrenome), c.sobrenome, "Dantas de Oliveria") as NOME
 FROM cliente c;
+
+
+SELECT
+	ROUND(c.codcli * 3.523232552, 0) as COD_CLI1,
+    ROUND(c.codcli * 3.523232552, 1) as COD_CLI2,
+    ROUND(c.codcli * 3.523232552, 2) as COD_CLI3,
+    ROUND(c.codcli * 3.523232552, 5) as COD_CLI1,
+    c.nome as NOME
+FROM cliente c;
+
+
+SELECT
+	c.codcli as COD_CLI,
+    c.nome as NOME
+FROM cliente c
+WHERE c.codcli MOD 2 = 0;
+
+
+SELECT
+	c.codcli as COD_CLI,
+    c.nome as NOME
+FROM cliente c
+WHERE ABS(c.codcli) > 0;
+
+
+SELECT
+	c.codcli as COD_CLI,
+    c.nome as NOME
+FROM cliente c
+WHERE SQRT(c.codcli) IN (1,2,3);
+
+
+
+/* DATE */
+
+/* IN ORACLE */
+SELECT sysdate
+FROM dual;
+/* IN MYSQL */
+SELECT NOW();
+
+
+/* USE A TABELA DUAL PARA FAZER CALCULOS */
+SELECT 300 * 10 FROM DUAL;
